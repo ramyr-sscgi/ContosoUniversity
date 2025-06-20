b473e1a40f12976fff93da5042df6c7f2317e4a7
Ramyr Christopher L. Garceniego <rlgarceniego@sscgi.com>
2025-06-20 14:50:36 +0800

    Update: update student entity to include annotations

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
@@ -8,15 +9,28 @@ namespace ContosoUniversity.Models
     {
         public int ID { get; set; }
 
+        [Required]
+        [StringLength(50)]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+        [Required]
+        [Column("FirstName")]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         [Display(Name = "First and Mid Name")]
         public string FirstMidName { get; set; }
 
+        [DataType(DataType.Date)]
         [Display(Name = "Enrollment Date")]
-        [DisplayFormat(DataFormatString = "{0:yyyy MMMM dd}")]
+        [DisplayFormat(DataFormatString = "{0:yyyy MMMM dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{LastName}, {FirstMidName}";
+            }
+        }
 
         public string Secret { get; set; }
 
